615fe71ff8ee1b9e55538ab139ea6145803d4464
yogtogz <yogeshgametime@gmail.com>
2026-09-22 20:36:00 -0500

    Update main.js

--- Teraxmas/main.js
+++ Teraxmas/main.js
@@ -4,8 +4,13 @@ async function definitionJsonLoader() {
     const entityStuff = await response.json();
     return entityStuff;
   } catch(er) {
+    alert("Error in async")
     alert(er);
+    console.warn("Unable to load json. Client will fail to be able to play :(");
+    console.warn(er);
+    return er;
   }
 }
 const entity = definitionJsonLoader();
+alert("Attempting Entity Json Load")
 alert(entity)
